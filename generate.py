5337cabdf904ddae7d2eaa84bd580ed23a3f4ccb
Lolenseu <98665691+lolenseu@users.noreply.github.com>
2022-03-13 00:17:18 +0800

    Add files via upload

---
+++ generate.py
@@ -0,0 +1,68 @@
+import os
+import time
+
+os.system('rm -rf src/xhash.txt && rm -rf src/xhashvar.txt && rm -rf src/xhashdict.txt && rm -rf src/xhash.json')
+time.sleep(1)
+
+alphalo = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphaup = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+spechar = [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"]
+numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0',]
+
+
+x = 0
+with open('src/xhash.txt', 'w') as f:
+    for i in alphalo + alphaup + numbers + spechar:
+        x += 1
+        if x > 9:
+            f.write("0x" + str(x) + " = " + i)
+            f.write('\n')
+        else:
+            f.write("0x" + str(0) + str(x) + " = " + i)
+            f.write('\n')
+
+
+x = 0
+with open('src/xhashvar.txt', 'w') as f:
+    for i in alphalo + alphaup + numbers + spechar:
+        x += 1
+        if x > 9:
+            f.write("0x" + str(x) + " = " + "\'" + i + "\'")
+            f.write('\n')
+        else:
+            f.write("0x" + str(0) + str(x) + " = " + "\'" + i + "\'")
+            f.write('\n')
+
+
+x = 0
+with open('src/xhashdict.txt', 'w') as f:
+    f.write("{")
+    f.write('\n')
+    for i in alphalo + alphaup + numbers + spechar:
+        x += 1
+        if x > 9:
+            f.write("   '0x" + str(x) + "'" + ": " + "\'" + i + "\'" + ",")
+            f.write('\n')
+        else:
+            f.write( "   '0x" + str(0) + str(x) + "'" + ": " + "\'" + i + "\'" + ",")
+            f.write('\n')
+    f.write("   '0x" + str(x) + "'" + ":" + "\'" + i + "\'")
+    f.write('\n')
+    f.write("}")
+
+
+x = 0
+with open('src/xhash.json', 'w') as f:
+    f.write("{")
+    f.write('\n')
+    for i in alphalo + alphaup + numbers + spechar:
+        x += 1
+        if x > 9:
+            f.write("   0x" + str(x) + ":" + "\'" + i + "\'" + ",")
+            f.write('\n')
+        else:
+            f.write("   0x" + str(0) + str(x) + ":" + "\'" + i + "\'" + ",")
+            f.write('\n')
+    f.write("   0x" + str(x) + ":" + "\'" + i + "\'")
+    f.write('\n')
+    f.write("}")
