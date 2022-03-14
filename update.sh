636196c39c1da6416d30e76441b184d49616abc8
Lolenseu <98665691+lolenseu@users.noreply.github.com>
2022-03-15 00:43:40 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 cd
-rm -rf xh
+rm -rf xhash
 sudo apt update && sudo apt upgrade -y
 sudo apt install python3 python3-pip git
-git clone https://github.com/lolenseu/xh.git
-cd xh
+git clone https://github.com/lolenseu/xhash.git
+cd xhash
 chmod +x update.sh
 clear
